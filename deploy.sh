4f83c7fb33beec1b554cfb7b122fa7a23ac5687d
shoaib989 <shoaib989@gmail.com>
2021-05-17 23:26:13 +0530

    edited deploy.sh from master branch

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,11 @@ lo...
 this is the file deploy.sh
 i have updated this file and i have also added the adder
 
+the biggest snake species is king cobra
+
+The largest animal in the world is the "Blue Whale"
+
+
+
 
 
--- deploy1.sh
+++
@@ -1,1 +0,0 @@
-hi this is deploy1.sh
