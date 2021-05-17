23126589b5650e7a8512a881b3902b9163e1f918
shoaib989 <shoaib989@gmail.com>
2021-05-17 19:40:45 +0530

    sending it

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+
 my name is shoaib
 my mom's name is Nikhat Sultana
 my father's name is Shaik Mahboob Subhani and he is a good man
+lo...
+this is the file deploy.sh
+i have updated this file
+
+
+
--- test.py
+++
@@ -1,4 +0,0 @@
-
-hi 
-this file is test.py
-
